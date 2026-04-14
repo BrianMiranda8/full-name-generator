fbff1dde4bd9b222a3feb06a4eec2301ad6f6e69
Brian Miranda <brianmirandamontiel@gmail.com>
2026-04-14 10:18:32 -0700

    using isEmpty() method instead of equals()

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -13,12 +13,12 @@ start();
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> names = new ArrayList<>();
-        String[] prompts = {"First Name", "Enter Middle Name", "Enter Last Name", "Enter Suffix"};
+        String[] prompts = {"First Name: ", "Enter Middle Name (Press Enter To Skip:) ", "Enter Last Name: ", "Enter Suffix (Press Enter To Skip:) "};
 
         System.out.println("Please enter your name");
 
         for (String prompt : prompts){
-            System.out.print(prompt + ": ");
+            System.out.print(prompt);
             String content = scanner.nextLine();
 
             names.add(content);
@@ -26,15 +26,17 @@ start();
 
         IO.println();
         IO.println("---------------- Your Name ---------------------");
+
+//        Loop through
         for (int i =0; i < names.size(); i++){
 
             String name = names.get(i);
             switch (i){
 
                 case 0 -> IO.println("First Name: " + name);
-                case 1 -> IO.println("Middle Name: " + (name.equalsIgnoreCase("") ? "No Middle Name" : name));
+                case 1 -> IO.println("Middle Name: " + (name.isEmpty() ? "No Middle Name" : name));
                 case 2 -> IO.println("Last Name: " + name );
-                case 3 -> IO.println("Suffix: " + (name.equalsIgnoreCase("") ? "" : name));
+                case 3 -> IO.println("Suffix: " + (name.isEmpty() ? "" : name));
 
             }
 
